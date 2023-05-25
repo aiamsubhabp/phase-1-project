982785cb61526038f9818b22a13ac993431d3b24
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-05-24 20:10:35 -0400

    create dropdown API call

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,10 @@
 ### Notes
 * i want to use this https://www.boredapi.com/
+* or this one to make quizzes https://opentdb.com/api_config.php
+* this one will have, change event... to change category.... https://www.youtube.com/watch?v=RS-t3TC2iUo
+* click events/submit to select the answer
+* and submit form, and see how many you got right
+* maybe key up event
 * the idea will be to come up with things to do when bored
 * can filter by participatnts, activity type etc,
 * can creata to do list, delete stuff, like activites etc
---
+++ js/index.js
@@ -0,0 +1,44 @@
+let dropdown = document.getElementById('drop-down')
+console.log(dropdown)
+
+
+// dropdown API call
+dropdown.addEventListener('change', (event) => {
+    let selectedDifficulty = event.target.value
+    fetch(`https://opentdb.com/api.php?amount=5&difficulty=${selectedDifficulty}&type=multiple`)
+    .then(res => res.json())
+    .then(data => {
+        let button = document.createElement('button')
+        button.innerText = 'abswerchasdf'
+        let answerContainer = document.getElementById('answer-choice')
+        answerContainer.append(button)
+        data.results.forEach(result => (
+            console.log(result.question)
+        ))
+
+    })
+
+})
+
+
+
+// let questions = []
+// const question = document.getElementById('question')
+// fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple')
+//     .then(res => res.json())
+//     .then(questionArrays => {
+//         console.log(questionArrays)
+//         questions = questionArrays.results.map(questionArray => {
+//             let formattedQuestion = {
+//                 question: questionArray.question
+//             }
+        
+//             console.log('formatted qystuiob', formattedQuestion)
+
+//         })
+//     })
+
+
+let response = {"response_code":0,"results":[{"category":"Sports","type":"multiple","difficulty":"easy","question":"In what sport is a &quot;shuttlecock&quot; used?","correct_answer":"Badminton","incorrect_answers":["Table Tennis","Rugby","Cricket"]},{"category":"Entertainment: Cartoon & Animations","type":"multiple","difficulty":"easy","question":"Which of these is NOT a Disney cartoon character?","correct_answer":"Daffy Duck","incorrect_answers":["Donald Duck","Daisy Duck","Scrooge McDuck"]},{"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"How many times do you fight Gilgamesh in &quot;Final Fantasy 5&quot;?","correct_answer":"6","incorrect_answers":["4","5","3"]},{"category":"Entertainment: Cartoon & Animations","type":"multiple","difficulty":"easy","question":"What is lost in Hawaiian and is also the name of a little girl in a 2002 film which features a alien named &quot;Stitch&quot;?","correct_answer":"Lilo","incorrect_answers":["Lolo","Lucy","Lulu"]},{"category":"Entertainment: Books","type":"multiple","difficulty":"easy","question":"What was Sir Handel&#039;s original name in &quot;The Railway Series&quot; and it&#039;s animated counterpart &quot;Thomas and Friends?&quot;","correct_answer":"Falcon","incorrect_answers":["Eagle","Kyte","Swallow"]}]}
+
+
